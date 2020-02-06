fac390946ad80a9eae72efbfec9f0debafc58c58
Jari Oksanen <jhoksane@gmail.com>
2020-02-06 09:52:09 +0200

    use new Fortran code in R with Matrix::sparseMatrix (works now)

--- R/readCEP.R
+++ R/readCEP.R
@@ -18,19 +18,24 @@
     ## source result: will return results in 'out'
     source(outfile)
     ## sanitize dimname
-    cnam <- names(out)
+    cnam <- out$jnames
     cnam <- gsub(" ", "", cnam)
     cnam <- make.names(cnam, unique = TRUE)
-    names(out) <- cnam
-    rnam <- rownames(out)
+    rnam <- out$inames
     rnam <- gsub(" ", "", rnam)
     rnam <- make.names(rnam, unique = TRUE)
-    rownames(out) <- rnam
+    ## Make a sparse matrix. It is trivial to make a dense matrix
+    ## manually (and we did so previously in Fortran code), but we
+    ## want to have an option of returning a Matrix::sparseMatrix
+    ## object.
+    out <- sparseMatrix(i = out$i, j = out$j, x = out$x,
+                        dimnames = list(rnam, cnam))
     if (positive) {
         rsum <- rowSums(out)
         csum <- colSums(out)
         if (any(rsum <= 0) || any(csum <= 0))
             out <- out[rsum > 0, csum > 0, drop = FALSE]
     }
+    out <- as.data.frame(as.matrix(out))
     out
 }
