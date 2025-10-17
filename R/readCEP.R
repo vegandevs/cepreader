047e89c53f3641de3fc8c2116d0655cfd291a710
Jari Oksanen <jhoksane@gmail.com>
2025-10-17 13:28:34 +0300

    readCEP is able to return data in "long" format
    Actually, this is similar to the internal format in readCEP(), but
    rows and columns are represented by their names.

--- R/readCEP.R
+++ R/readCEP.R
@@ -1,7 +1,10 @@
 `readCEP` <-
     function (file, maxdata = 10000, positive = TRUE, sparseMatrix = FALSE,
-              ...)
+              long = FALSE, ...)
 {
+    ## check allowed formats
+    if (sparseMatrix && long)
+        stop("only one of 'long' and 'sparseMatrix' can be TRUE")
     ## launch external binary to write R input data
     cepread <- file.path(path.package("cepreader"),
                                       "bin", "cepread")
@@ -26,19 +29,30 @@
     rnam <- out$inames
     rnam <- gsub(" ", "", rnam)
     rnam <- make.names(rnam, unique = TRUE)
-    ## Make a sparse matrix. It is trivial to make a dense matrix
-    ## manually (and we did so previously in Fortran code), but we
-    ## want to have an option of returning a Matrix::sparseMatrix
-    ## object.
-    out <- sparseMatrix(i = out$i, j = out$j, x = out$x,
-                        dimnames = list(rnam, cnam), ...)
-    if (positive) {
-        rsum <- rowSums(out)
-        csum <- colSums(out)
-        if (any(rsum <= 0) || any(csum <= 0))
-            out <- out[rsum > 0, csum > 0, drop = FALSE]
+    ## Output is either in "long" format of data triplets (row name,
+    ## column name, data value) or a matrix-like rows x columns
+    ## table. Internally data are already in triplets and needs only
+    ## index numbers changed to names. Normal matrix and data.frame is
+    ## made via sparseMatrix.
+    if (long) {
+        out <- data.frame("row" = rnam[out$i], "col" = cnam[out$j],
+                          "val" = out$x, row.names = NULL)
+        if (positive) {
+            if (any(out$val <= 0))
+                out <- out[out$val > 0,]
+        }
+    } else {
+        ## always make sparseMatrix first
+        out <- sparseMatrix(i = out$i, j = out$j, x = out$x,
+                            dimnames = list(rnam, cnam), ...)
+        if (positive) {
+            rsum <- rowSums(out)
+            csum <- colSums(out)
+            if (any(rsum <= 0) || any(csum <= 0))
+                out <- out[rsum > 0, csum > 0, drop = FALSE]
+        } ## change to data.frame of dense matrix
+        if (!sparseMatrix)
+            out <- as.data.frame(as.matrix(out))
     }
-    if (!sparseMatrix)
-        out <- as.data.frame(as.matrix(out))
     out
 }
